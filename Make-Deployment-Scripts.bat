37ac3dc96a8f6ef67ce3bc6e6fc19bffc59ac68c
Kevin Barnes <kbarnes3@gmail.com>
2016-10-15 12:30:00 -0700

    Removing slashes from versions

--- Make-Deployment-Scripts.bat
+++ Make-Deployment-Scripts.bat
@@ -8,16 +8,16 @@ mkdir %DESTINATION%\BIOS\Install\RS1
 mkdir %DESTINATION%\UEFI\Install\Blue
 mkdir %DESTINATION%\UEFI\Install\RS1
 
-cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Home %HOMEDESTNAME%
+cmd /c %~dp0\Make-SKU-Deployments.bat RS1 "%DESTINATION%" Home %HOMEDESTNAME%
 REM cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Surface-3-Home %HOMESURFACE3DESTNAME% /NOBIOS
-cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Pro %PRODESTNAME%
+cmd /c %~dp0\Make-SKU-Deployments.bat RS1 "%DESTINATION%" Pro %PRODESTNAME%
 REM cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Surface-3-Pro %PROSURFACE3DESTNAME% /NOBIOS
 REM cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Surface-Pro-3-Pro %PROSURFACEPRO3DESTNAME% /NOBIOS
 REM cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Surface-Pro-4-Pro %PROSURFACEPRO4DESTNAME% /NOBIOS
 REM cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Surface-Book-Pro %PROSURFACEBOOKDESTNAME% /NOBIOS
-cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Enterprise %ENTERPRISEDESTNAME%
+cmd /c %~dp0\Make-SKU-Deployments.bat RS1 "%DESTINATION%" Enterprise %ENTERPRISEDESTNAME%
 REM cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Surface-3-Enterprise %ENTERPRISESURFACE3DESTNAME% /NOBIOS
 REM cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Surface-Pro-3-Enterprise %ENTERPRISESURFACEPRO3DESTNAME% /NOBIOS
 REM cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Surface-Pro-4-Enterprise %ENTERPRISESURFACEPRO4DESTNAME% /NOBIOS
 REM cmd /c %~dp0\Make-SKU-Deployments.bat /RS1 "%DESTINATION%" Surface-Book-Enterprise %ENTERPRISESURFACEBOOKDESTNAME% /NOBIOS
-cmd /c %~dp0\Make-SKU-Deployments.bat /BLUE "%DESTINATION%" Server %SERVERSTANDARDDESTNAME%
+cmd /c %~dp0\Make-SKU-Deployments.bat BLUE "%DESTINATION%" Server %SERVERSTANDARDDESTNAME%
--- Make-SKU-Deployments.bat
+++ Make-SKU-Deployments.bat
@@ -7,12 +7,12 @@ SET PREFIX=%~3
 SET IMAGENAME=%~4
 SET NOBIOS=%~5
 
-IF "%VERSION%"=="/RS1" IF NOT "%NOBIOS%"=="/NOBIOS" ECHO ..\Helpers\RS1-Install-BIOS.bat "%IMAGENAME%" > "%DESTINATION%\BIOS\Install\%VERSION%\%PREFIX%-BIOS-Default.bat"
-IF "%VERSION%"=="/RS1" IF NOT "%NOBIOS%"=="/NOBIOS" ECHO ..\Helpers\RS1-Install-BIOS.bat "%IMAGENAME%" /Compact > "%DESTINATION%\BIOS\Install\%VERSION%\%PREFIX%-BIOS-Compact.bat"
-IF "%VERSION%"=="/RS1" ECHO ..\Helpers\RS1-Install-UEFI.bat "%IMAGENAME%" > "%DESTINATION%\UEFI\Install\%VERSION%\%PREFIX%-UEFI-Default.bat"
-IF "%VERSION%"=="/RS1" ECHO ..\Helpers\RS1-Install-UEFI.bat "%IMAGENAME%" /Compact > "%DESTINATION%\UEFI\Install\%VERSION%\%PREFIX%-UEFI-Compact.bat"
+IF "%VERSION%"=="RS1" IF NOT "%NOBIOS%"=="/NOBIOS" ECHO ..\Helpers\RS1-Install-BIOS.bat "%IMAGENAME%" > "%DESTINATION%\BIOS\Install\%VERSION%\%PREFIX%-BIOS-Default.bat"
+IF "%VERSION%"=="RS1" IF NOT "%NOBIOS%"=="/NOBIOS" ECHO ..\Helpers\RS1-Install-BIOS.bat "%IMAGENAME%" /Compact > "%DESTINATION%\BIOS\Install\%VERSION%\%PREFIX%-BIOS-Compact.bat"
+IF "%VERSION%"=="RS1" ECHO ..\Helpers\RS1-Install-UEFI.bat "%IMAGENAME%" > "%DESTINATION%\UEFI\Install\%VERSION%\%PREFIX%-UEFI-Default.bat"
+IF "%VERSION%"=="RS1" ECHO ..\Helpers\RS1-Install-UEFI.bat "%IMAGENAME%" /Compact > "%DESTINATION%\UEFI\Install\%VERSION%\%PREFIX%-UEFI-Compact.bat"
 
-IF "%VERSION%"=="/BLUE" IF NOT "%NOBIOS%"=="/NOBIOS" ECHO ..\Helpers\Blue-Install-BIOS-Default.bat "%IMAGENAME%" > "%DESTINATION%\BIOS\Install\%VERSION%\%PREFIX%-BIOS-Default.bat"
-IF "%VERSION%"=="/BLUE" ECHO ..\Helpers\Blue-Install-UEFI-Default.bat "%IMAGENAME%" > "%DESTINATION%\UEFI\Install\%VERSION%\%PREFIX%-UEFI-Default.bat"
+IF "%VERSION%"=="BLUE" IF NOT "%NOBIOS%"=="/NOBIOS" ECHO ..\Helpers\Blue-Install-BIOS-Default.bat "%IMAGENAME%" > "%DESTINATION%\BIOS\Install\%VERSION%\%PREFIX%-BIOS-Default.bat"
+IF "%VERSION%"=="BLUE" ECHO ..\Helpers\Blue-Install-UEFI-Default.bat "%IMAGENAME%" > "%DESTINATION%\UEFI\Install\%VERSION%\%PREFIX%-UEFI-Default.bat"
 
 ECHO %PREFIX% deployment scripts created
