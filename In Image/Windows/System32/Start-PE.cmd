2aafced3321dcf5a19a6dacaf3bcbd2e97637a2c
Kevin Barnes <kbarnes3@gmail.com>
2017-08-07 15:42:45 -0700

    Running powershell when booting

--- In Image/Windows/System32/Start-PE.cmd
+++ In Image/Windows/System32/Start-PE.cmd
@@ -1,19 +1,4 @@
 @ECHO OFF
 
-SET DriveLetter=unknown
-for /f "tokens=2,*" %%a in ('reg query HKLM\system\currentcontrolset\control /v PEBootRamDiskSourceDrive ^| find /i "PEBootRamDiskSourceDrive"') do set DriveLetter=%%b
-
-ECHO Boot drive is %DriveLetter%
-
-if "%DriveLetter%"=="unknown" GOTO :Unknown
-GOTO :Known
-
-:Unknown
-Call List-Volume.bat
-GOTO :EOF
-
-:Known
-Call %DriveLetter%Scripts\Start.bat
-GOTO :EOF
-
-:EOF
+powershell.exe "Set-ExecutionPolicy Unrestricted"
+powershell.exe .\Start-PE.ps1
