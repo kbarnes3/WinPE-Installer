4eb2390405af9d90fc1f4067256f2a1995a99b31
Kevin Barnes <kbarnes3@gmail.com>
2017-10-03 12:56:56 -0700

    Support VHDs on BIOS systems

--- On Disk/Scripts/Start.ps1
+++ On Disk/Scripts/Start.ps1
@@ -15,6 +15,9 @@ Set-Location "$($FirmwareMode)\Partition"
 diskpart /s \DiskPart\List-Disk.txt
 Write-Host "To partition your disk run:"
 Write-Host "    RS-Partition-$($FirmwareMode).ps1 -DiskNumber n // For Windows 10 Version 1703 and Windows Server 2016"
+if ($FirmwareMode -eq "BIOS") {
+    Write-Host "    RS-Partition-BIOS-VHD.ps1 -DiskNumber n -VhdName Filename // If you know what you are doing"
+}
 if ($FirmwareMode -eq "UEFI") {
     Write-Host "    RS-Partition-UEFI-VHDX.ps1 -DiskNumber n -VhdxName Filename // If you know what you are doing"
 }
