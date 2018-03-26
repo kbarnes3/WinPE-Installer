07f6f4ea21af539f7112ed7d06825c7eff863bf8
Kevin Barnes <kbarnes3@gmail.com>
2018-03-26 10:49:33 -0700

    Updating cumulative updates and drivers

--- SourceFiles.psm1
+++ SourceFiles.psm1
@@ -11,11 +11,11 @@ function Get-ServerIsoPath {
 }
 
 function Get-RS1CumulativeUpdatePath {
-    return "E:\Big Stuff\Discs\Cumulative Updates\2018-03 Cumulative Update for Windows Server 2016 for x64-based Systems (KB4088787)\windows10.0-kb4088787-x64_76e9d6684e0a004f51ea7373e4ea6217193c1b5e.msu"
+    return "E:\Big Stuff\Discs\Cumulative Updates\2018-03 Cumulative Update for Windows Server 2016 for x64-based Systems (KB4088889)\windows10.0-kb4088889-x64_eee5b352932e9d20d228f56b74dd4c23313d2955.msu"
 }
 
 function Get-RS3CumulativeUpdatePath {
-    return "E:\Big Stuff\Discs\Cumulative Updates\2018-03 Cumulative Update for Windows 10 Version 1709 for x64-based Systems (KB4088776)\windows10.0-kb4088776-x64_55756340f1e2c2090f94de6d256eafd75e1cee9c.msu"
+    return "E:\Big Stuff\Discs\Cumulative Updates\2018-03 Cumulative Update for Windows 10 Version 1709 for x64-based Systems (KB4089848)\windows10.0-kb4089848-x64_db7c5aad31c520c6983a937c3d53170e84372b11.msu"
 }
 
 function Get-SurfaceProDrivers {
@@ -31,7 +31,7 @@ function Get-Surface3Drivers {
 }
 
 function Get-SurfacePro3Drivers {
-    return "E:\Big Stuff\Discs\SurfacePro3_Win10_15063_1704002_0.msi"
+    return "E:\Big Stuff\Discs\SurfacePro3_Win10_15063_1801002_3.msi"
 }
 
 function Get-SurfacePro4Drivers {
@@ -39,7 +39,7 @@ function Get-SurfacePro4Drivers {
 }
 
 function Get-SurfaceBookDrivers {
-    return "E:\Big Stuff\Discs\SurfaceBook_Win10_16299_1801000_0.msi"
+    return "E:\Big Stuff\Discs\SurfaceBook_Win10_16299_1801500_3.msi"
 }
 
 function Get-SurfaceStudioDrivers {
@@ -51,7 +51,7 @@ function Get-SurfacePro2017Drivers {
 }
 
 function Get-SurfaceLaptopDrivers {
-    "E:\Big Stuff\Discs\SurfaceLaptop_Win10_15063_1801008_1.msi"
+    return "E:\Big Stuff\Discs\SurfaceLaptop_Win10_15063_1802008_1.msi"
 }
 
 function Get-SurfaceBook2Drivers {
