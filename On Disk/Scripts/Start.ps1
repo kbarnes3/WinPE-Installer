aed1e360a5dfae18f917e0e6243ac276c11cab79
Kevin Barnes <kbarnes3@gmail.com>
2019-08-21 16:00:02 -0700

    Printing bus types on hard drives

--- On Disk/Scripts/Start.ps1
+++ On Disk/Scripts/Start.ps1
@@ -13,6 +13,15 @@ Write-Host "The PC is booted in $FirmwareMode mode"
 
 Set-Location "$($FirmwareMode)\Partition"
 diskpart /s \DiskPart\List-Disk.txt
+Get-PhysicalDisk | Format-Table @{Name="DiskNumber"; Expression={$_.DeviceId}},
+    FriendlyName,
+    SerialNumber,
+    BusType,
+    @{Name="Size"; Expression={
+        $size = $_.Size;
+        $postfixes = @( "B", "KB", "MB", "GB", "TB", "PB" );
+        for ($i=0; $size -ge 1024 -and $i -lt $postfixes.Length; $i++) { $size = $size / 1024; };
+        return "" + [System.Math]::Round($size,2) + " " + $postfixes[$i];}}
 Write-Host "To partition your disk run:"
 Write-Host "    .\RS-Partition-$($FirmwareMode).ps1 -DiskNumber n " -ForegroundColor Yellow -NoNewline 
 Write-Host "// For Windows 10 Version 1903 and Windows Server 2019"
