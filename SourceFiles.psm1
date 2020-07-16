1082017bd9bef753d35262bd809986752ac159f1
Kevin Barnes <kbarnes3@gmail.com>
2020-07-16 11:28:45 -0700

    Updating to 7B and new drivers

--- SourceFiles.psm1
+++ SourceFiles.psm1
@@ -11,19 +11,19 @@ function Get-ServerIsoPath {
 }
 
 function Get-RS5ServicingStackUpdatePath {
-    return "$env:DISC_PATH\Cumulative Updates\2020-06 Servicing Stack Update for Windows Server 2019 for x64-based Systems (KB4562562)\windows10.0-kb4562562-x64_85f4082ff8a2f8bd870495fe864b00306322d0ea.msu"
+    return "$env:DISC_PATH\Cumulative Updates\2020-07 Servicing Stack Update for Windows Server 2019 for x64-based Systems (KB4558997)\windows10.0-kb4558997-x64_9c9a7137d609ef46898bba95e204e6c867a9aa12.msu"
 }
 
 function Get-RS5CumulativeUpdatePath {
-    return "$env:DISC_PATH\Cumulative Updates\2020-06 Cumulative Update for Windows Server 2019 for x64-based Systems (KB4561608)\windows10.0-kb4561608-x64_31903b9b843877474d625e2b3beb5fed3b8abc3a.msu"
+    return "$env:DISC_PATH\Cumulative Updates\2020-07 Cumulative Update for Windows Server 2019 for x64-based Systems (KB4558998)\windows10.0-kb4558998-x64_6da68fe659dacb747458ab3a431c3546ce7765b5.msu"
 }
 
 function Get-ServicingStackUpdatePathVb {
-    return "$env:DISC_PATH\Cumulative Updates\2020-06 Servicing Stack Update for Windows 10 Version 2004 for x64-based Systems (KB4560366)\windows10.0-kb4560366-x64_653c88cde27a1adff38843c1b054fbbcef380b6f.msu"
+    return "$env:DISC_PATH\Cumulative Updates\2020-07 Servicing Stack Update for Windows 10 Version 2004 for x64-based Systems (KB4566785)\windows10.0-kb4566785-x64_f70d62d77582f5fb67d9177f82714f49c83f5892.msu"
 }
 
 function Get-CumulativeUpdatePathVb {
-    return "$env:DISC_PATH\Cumulative Updates\2020-06 Cumulative Update for Windows 10 Version 2004 for x64-based Systems (KB4557957)\windows10.0-kb4557957-x64_ce530e139ad0d12d33f185a5db868248110b93eb.msu"
+    return "$env:DISC_PATH\Cumulative Updates\2020-07 Cumulative Update for Windows 10 Version 2004 for x64-based Systems (KB4565503)\windows10.0-kb4565503-x64_db6e183196b0c394a7fa3be6e8fd26544c6dc9ce.msu"
 }
 
 function Get-SurfaceProDrivers {
@@ -55,7 +55,7 @@ function Get-SurfaceStudioDrivers {
 }
 
 function Get-SurfacePro2017Drivers {
-    return "$env:DISC_PATH\Drivers\SurfacePro_Win10_18362_20.033.37286.0.msi"
+    return "$env:DISC_PATH\Drivers\SurfacePro_Win10_18362_20.054.41646.0.msi"
 }
 
 function Get-SurfaceLaptopDrivers {
@@ -63,7 +63,7 @@ function Get-SurfaceLaptopDrivers {
 }
 
 function Get-SurfaceBook2Drivers {
-    return "$env:DISC_PATH\Drivers\SurfaceBook2_Win10_18362_20.041.10163.0.msi"
+    return "$env:DISC_PATH\Drivers\SurfaceBook2_Win10_18362_20.053.37335.0.msi"
 }
 
 function Get-SurfaceGoDrivers {
@@ -75,23 +75,23 @@ function Get-SurfaceLaptop2Drivers {
 }
 
 function Get-SurfacePro6Drivers {
-    return "$env:DISC_PATH\Drivers\SurfacePro6_Win10_18362_20.033.37287.0.msi"
+    return "$env:DISC_PATH\Drivers\SurfacePro6_Win10_18362_20.054.41487.0.msi"
 }
 
 function Get-SurfaceStudio2Drivers {
-    return "$env:DISC_PATH\Drivers\SurfaceStudio2_Win10_18362_19.122.25411.0.msi"
+    return "$env:DISC_PATH\Drivers\SurfaceStudio2_Win10_18362_20.052.18795.0.msi"
 }
 
 function Get-SurfacePro7Drivers {
-    return "$env:DISC_PATH\Drivers\SurfacePro7_Win10_18362_20.014.39957.0.msi"
+    return "$env:DISC_PATH\Drivers\SurfacePro7_Win10_18362_20.051.10390.0.msi"
 }
 
 function Get-SurfaceLaptop3IntelDrivers {
-    return "$env:DISC_PATH\Drivers\SurfaceLaptop3_intel_Win10_18362_20.010.3582.0.msi"
+    return "$env:DISC_PATH\Drivers\SurfaceLaptop3_intel_Win10_18362_20.052.21137.0.msi"
 }
 
 function Get-SurfaceLaptop3AmdDrivers {
-    return "$env:DISC_PATH\Drivers\SurfaceLaptop3_amd_Win10_18362_20.050.1599.0.msi"
+    return "$env:DISC_PATH\Drivers\SurfaceLaptop3_amd_Win10_18362_20.053.37902.0.msi"
 }
 
 function Get-SurfaceBook3Drivers {
@@ -99,7 +99,7 @@ function Get-SurfaceBook3Drivers {
 }
 
 function Get-SurfaceGo2Drivers {
-    return "$env:DISC_PATH\Drivers\SurfaceGo2_Win10_18362_2000202_0.msi"
+    return "$env:DISC_PATH\Drivers\SurfaceGo2_Win10_18362_20.063.23730_0.msi"
 }
 
 function Get-IntelRapidStorageDrivers {
