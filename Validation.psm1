ea4c2ce1cdafbc8dfdccdfbadd303db8365af288
Kevin Barnes <kbarnes3@gmail.com>
2026-05-07 11:54:59 -0700

    Print all validation errors before aborting, not just the first one

--- Validation.psm1
+++ Validation.psm1
@@ -11,59 +11,84 @@ function Confirm-Environment {
 
     $env:Path += ";C:\Program Files\7-Zip\"
 
-    $SourceFiles =
-        ("R:\"),
-        (Get-ConsumerIsoPath),
-        (Get-BusinessIsoPath),
-        (Get-ServerIsoPath),
-        (Get-CumulativeUpdatePathGe),
-        (Get-CumulativeUpdatePathGeServer),
-        (Get-AmdRZ616WifiDrivers),
-        (Get-AmdRZ717WifiDrivers),
-        (Get-IntelNicDrivers),
-        (Get-IntelWifiDrivers),
-        (Get-MarvellNicDrivers),
-        (Get-SurfaceBook2Drivers),
-        (Get-SurfaceBook3Drivers),
-        (Get-SurfaceGo2Drivers),
-        (Get-SurfaceGo3Drivers),
-        (Get-SurfaceGo4Drivers),
-        (Get-SurfaceLaptop2Drivers),
-        (Get-SurfaceLaptop3IntelDrivers),
-        (Get-SurfaceLaptop3AmdDrivers),
-        (Get-SurfaceLaptop4IntelDrivers),
-        (Get-SurfaceLaptop4AmdDrivers),
-        (Get-SurfaceLaptop5Drivers),
-        (Get-SurfaceLaptop6ForBusinessDrivers),
-        (Get-SurfaceLaptopForBusiness7Drivers),
-        (Get-SurfaceLaptopGoDrivers),
-        (Get-SurfaceLaptopGo2Drivers),
-        (Get-SurfaceLaptopGo3Drivers),
-        (Get-SurfaceLaptopStudioDrivers),
-        (Get-SurfaceLaptopStudio2Drivers),
-        (Get-SurfacePro6Drivers),
-        (Get-SurfacePro7Drivers),
-        (Get-SurfacePro7PlusDrivers),
-        (Get-SurfacePro8Drivers),
-        (Get-SurfacePro9Drivers),
-        (Get-SurfacePro10ForBusinessDrivers),
-        (Get-SurfaceProForBusiness11Drivers),
-        (Get-SurfaceStudio2Drivers),
-        (Get-SurfaceStudio2PlusDrivers),
-        (Get-IntelRapidStorageDrivers)
-
-    if (-Not $IgnoreWinPEDriverDir) {
-        $SourceFiles += (Get-WinPEDriverDir)
+    $SourceFileAccessors = [ordered]@{
+        'R:\'                                  = { 'R:\' }
+        'Get-ConsumerIsoPath'                  = { Get-ConsumerIsoPath }
+        'Get-BusinessIsoPath'                  = { Get-BusinessIsoPath }
+        'Get-ServerIsoPath'                    = { Get-ServerIsoPath }
+        'Get-CumulativeUpdatePathGe'           = { Get-CumulativeUpdatePathGe }
+        'Get-CumulativeUpdatePathGeServer'     = { Get-CumulativeUpdatePathGeServer }
+        'Get-AmdRZ616WifiDrivers'              = { Get-AmdRZ616WifiDrivers }
+        'Get-AmdRZ717WifiDrivers'              = { Get-AmdRZ717WifiDrivers }
+        'Get-IntelNicDrivers'                  = { Get-IntelNicDrivers }
+        'Get-IntelWifiDrivers'                 = { Get-IntelWifiDrivers }
+        'Get-MarvellNicDrivers'                = { Get-MarvellNicDrivers }
+        'Get-SurfaceBook2Drivers'              = { Get-SurfaceBook2Drivers }
+        'Get-SurfaceBook3Drivers'              = { Get-SurfaceBook3Drivers }
+        'Get-SurfaceGo2Drivers'                = { Get-SurfaceGo2Drivers }
+        'Get-SurfaceGo3Drivers'                = { Get-SurfaceGo3Drivers }
+        'Get-SurfaceGo4Drivers'                = { Get-SurfaceGo4Drivers }
+        'Get-SurfaceLaptop2Drivers'            = { Get-SurfaceLaptop2Drivers }
+        'Get-SurfaceLaptop3IntelDrivers'       = { Get-SurfaceLaptop3IntelDrivers }
+        'Get-SurfaceLaptop3AmdDrivers'         = { Get-SurfaceLaptop3AmdDrivers }
+        'Get-SurfaceLaptop4IntelDrivers'       = { Get-SurfaceLaptop4IntelDrivers }
+        'Get-SurfaceLaptop4AmdDrivers'         = { Get-SurfaceLaptop4AmdDrivers }
+        'Get-SurfaceLaptop5Drivers'            = { Get-SurfaceLaptop5Drivers }
+        'Get-SurfaceLaptop6ForBusinessDrivers' = { Get-SurfaceLaptop6ForBusinessDrivers }
+        'Get-SurfaceLaptopForBusiness7Drivers' = { Get-SurfaceLaptopForBusiness7Drivers }
+        'Get-SurfaceLaptopGoDrivers'           = { Get-SurfaceLaptopGoDrivers }
+        'Get-SurfaceLaptopGo2Drivers'          = { Get-SurfaceLaptopGo2Drivers }
+        'Get-SurfaceLaptopGo3Drivers'          = { Get-SurfaceLaptopGo3Drivers }
+        'Get-SurfaceLaptopStudioDrivers'       = { Get-SurfaceLaptopStudioDrivers }
+        'Get-SurfaceLaptopStudio2Drivers'      = { Get-SurfaceLaptopStudio2Drivers }
+        'Get-SurfacePro6Drivers'               = { Get-SurfacePro6Drivers }
+        'Get-SurfacePro7Drivers'               = { Get-SurfacePro7Drivers }
+        'Get-SurfacePro7PlusDrivers'           = { Get-SurfacePro7PlusDrivers }
+        'Get-SurfacePro8Drivers'               = { Get-SurfacePro8Drivers }
+        'Get-SurfacePro9Drivers'               = { Get-SurfacePro9Drivers }
+        'Get-SurfacePro10ForBusinessDrivers'   = { Get-SurfacePro10ForBusinessDrivers }
+        'Get-SurfaceProForBusiness11Drivers'   = { Get-SurfaceProForBusiness11Drivers }
+        'Get-SurfaceStudio2Drivers'            = { Get-SurfaceStudio2Drivers }
+        'Get-SurfaceStudio2PlusDrivers'        = { Get-SurfaceStudio2PlusDrivers }
+        'Get-IntelRapidStorageDrivers'         = { Get-IntelRapidStorageDrivers }
     }
 
-    $SourceFiles | ForEach-Object {
-        if (-Not ($_))
-        {
-            throw "Path is null"
+    if (-Not $IgnoreWinPEDriverDir) {
+        $SourceFileAccessors['Get-WinPEDriverDir'] = { Get-WinPEDriverDir }
+    }
+
+    $errors = New-Object System.Collections.Generic.List[string]
+
+    foreach ($name in $SourceFileAccessors.Keys) {
+        $accessor = $SourceFileAccessors[$name]
+        $path = $null
+        try {
+            $path = & $accessor
         }
-        if (-Not (Test-Path $_)) {
-            throw "Unable to find $_"
+        catch {
+            $message = "$name failed: $_"
+            $errors.Add($message)
+            Write-Warning $message
+            continue
         }
+
+        if (-Not ($path)) {
+            $message = "$name returned a null or empty path"
+            $errors.Add($message)
+            Write-Warning $message
+            continue
+        }
+
+        if (-Not (Test-Path $path)) {
+            $message = "Unable to find $path (from $name)"
+            $errors.Add($message)
+            Write-Warning $message
+            continue
+        }
+    }
+
+    if ($errors.Count -gt 0) {
+        throw "Confirm-Environment found $($errors.Count) validation error(s); see preceding error output for details."
     }
 
     return $true
